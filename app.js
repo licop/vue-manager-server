2ea7d23c0b37300363cbcf4d0d026af012bfdaf6
licop <licop@users.noreply.github.com>
2023-02-21 16:01:38 +0800

    feat: 文档

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-
+const log4js = require('./utils/log')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
@@ -17,7 +16,6 @@ app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -26,10 +24,9 @@ app.use(views(__dirname + '/views', {
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
   await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+
+  log4js.info('log output')
 })
 
 // routes
@@ -37,8 +34,8 @@ app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+app.on('error', (err, ctx) => {  
+  log4js.error(`${err.stack}`)
 });
 
 module.exports = app
