0d7114055156ab752f5fa27f3d749288ee0041f5
licop <licop@users.noreply.github.com>
2023-02-27 10:57:06 +0800

    feat: 用户管理

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,14 @@
 
 const router = require('koa-router')()
 const User = require('../models/userSchema')
+const Counter = require('../models/counterSchema')
 const util = require('../utils/util')
+const md5 = require('md5')
 const jwt = require('jsonwebtoken')
 
 router.prefix('/users')
 
+// 用户登录
 router.post('/login', async (ctx, next) => {
 
  try {
@@ -38,6 +41,101 @@ router.post('/login', async (ctx, next) => {
     }
   } catch (err) {
     ctx.body = util.fail(err.msg)
+  }
+})
+
+// 用户列表
+router.get('/list',async (ctx) => {
+  const { userName, userId, state } = ctx.request.query
+  const { page, skipIndex } = util.pager(ctx.request.query)
+  
+  let params = {}
+
+  if(userId) params.userId = userId
+  if(userName) params.userName = userName
+  if(state && state != '0') params.state = state
+
+  try {
+    // 根据条件查询所有的用户列表
+    const query = User.find(params, { _id: 0, userPwd: 0 })
+    const list = await query.skip(skipIndex).limit(page.pageSize)
+    const total = await User.countDocuments(params)
+
+    ctx.body = util.success({
+      page: {
+        ...page,
+        total
+      },
+      list
+    })
+  } catch (error) {
+    ctx.body = util.fail(`查询异常：${error.stack}`)
+  }
+
+})
+
+// 用户删除/批量删除
+router.post('/delete', async (ctx) => {
+  // 待删除的用户Id数组
+  const { userIds } = ctx.request.body
+  const res = await User.updateMany({ userId: { $in: userIds } }, { state: 2 })
+  if (res.modifiedCount > 0) {
+    res.nModified = res.modifiedCount
+    ctx.body = util.success(res, `共删除成功${res.modifiedCount}条`)
+  } else {
+    ctx.body = util.fail('删除失败');
+  }
+})
+
+// 用户新增/编辑
+router.post('/operate', async (ctx) => {
+  const { userId, userName, userEmail, job, state, roleList, deptId, mobile, action } = ctx.request.body
+  
+  if(action === 'add') {
+    if(!userName || !userEmail || !deptId) {
+      ctx.body = util.fail('参数错误', util.CODE.PARAM_ERROR)
+      return
+    } 
+    const res = await User.findOne({ $or: [{ userName }, { userEmail }] }, '_id userName userEmail')
+    if(res) {
+      ctx.body = util.fail(`系统监测到有重复的用户，信息如下：${res.userName} - ${res.userEmail}`)
+    } else {
+      const doc = await Counter.findOneAndUpdate({ _id: 'userId' }, { $inc: { sequence_value: 1 } }, { new: true })
+      
+      try {
+        const user = new User({
+          userId: doc.sequence_value,
+          userName,
+          userEmail,
+          userPwd: md5('123456'),
+          role: 1, // 默认普通用户
+          roleList,
+          job,
+          state,
+          deptId,
+          mobile
+        })
+
+        user.save()
+        ctx.body = util.success('', '用户创建成功')
+      
+      } catch (error) {
+        ctx.body = util.fail(error.stack, '用户创建失败')
+      }
+    }
+  } else {
+    if(!deptId) {
+      ctx.body = util.fail('部门不能为空', util.CODE.PARAM_ERROR)
+      return
+    }
+
+    try {
+      const res = await User.findOneAndUpdate({ userId }, { job, state, roleList, deptId, mobile })
+      ctx.body = util.success('', '更新成功')
+    } catch (error) {
+      ctx.body = util.fail(error.stack, '更新失败')
+    }
+    
   }
 })
 
